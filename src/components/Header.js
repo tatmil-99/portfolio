ade8c723fe2be1324f378159cf7e383a296edaa2
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-08-30 17:35:48 -0500

    removes symbol and makes base positioning mobile responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,39 +4,16 @@ import styled from 'styled-components';
 const HeaderDiv = styled.div`
   background-color: blue;
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
+  flex-direction: row;
   height: 25%;
   width: 100%;
-  padding-left: 25px;
   
-`;
-
-const SymbolTop = styled.div`
-  display: block;
-  width: 75px;
-  height: 25px;
-  background-color: silver;
-  position: relative;
-  left: 0;
-  right: 0;
-`;
-
-const SymbolBottom = styled.div`
-  display: block;
-  width: 25px;
-  height: 85px;
-  background-color: black;
-  position: relative;
-  left: 25px;
 `;
 
 const Header = () => {
   return (
     <HeaderDiv>
-      <SymbolTop />
-      <SymbolBottom />
+      
     </HeaderDiv>
   )
 }
