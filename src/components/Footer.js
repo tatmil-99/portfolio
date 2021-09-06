20326570ecfb580d99bfaa82fe239bea2cb59d44
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-09-05 22:47:25 -0500

    adds some styling and positioning code to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,25 @@ const FooterDiv = styled.div`
   background-color: rgb(47, 48, 59);
   display: flex;
   flex-direction: row;
-  height: 25%;
+  justify-content: center;
+  align-items: center;
   width: 100%;
+  margin: 50px 0px 0px 0px;
+  color: rgb(107, 122, 144);
 
-  /* @media (max-width: 415px) {
+    /* @media (max-width: 415px) {
     margin-bottom: 15px;
   } */
+  
+  p {
+    margin: 50px 50px;
+  }
 `;
 
 const Footer = () => {
   return (
     <FooterDiv>
-      
+      <p>Created by Tatien Miller</p>
     </FooterDiv>
   )
 }
