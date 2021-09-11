b603a20b44c52f8b59deaf05e2b5455cf62c44b6
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-09-10 21:23:41 -0500

    creates project section jsx structure

--- src/components/Main.js
+++ src/components/Main.js
@@ -32,6 +32,22 @@ const StoryDiv = styled(FlexExtended)`
   }
 `;
 
+const projectsContainer = styled(FlexExtended)`
+
+`;
+
+const projectContent = styled(FlexExtended)`
+
+`;
+
+const projectDescription = styled(FlexExtended)`
+  flex-direction: row;
+`;
+
+const projectLinks = styled(FlexExtended)`
+  flex-direction: row;
+`;
+
 const Main = () => {
   return (
     <MainDiv>
@@ -39,6 +55,18 @@ const Main = () => {
         <h2>My Digital Story</h2>
         <p> My name is Tatien Miller and I am a certified full-stack developer -- with an emphasis in front-end development -- for now. My digital journey began in college, pre-covid, where I was studying business. Listed in the required courses to take was Computer Science 101. I quickly discovered I loved solving the cryptic coding errors that would arise in homework. Unfortunately, this experience was cut short by Covid. It was then after this I decided to attend a virtual coding bootcamp. Although hard, bootcamp solidified my interest in programming and web development. Since my graduation, my time has been spent learning and building projects... like this portfolio you are viewing!</p>
       </StoryDiv>
+      <projectsContainer>
+        <h2>Projects</h2>
+        <projectContent>
+          <h3>The Bean</h3>
+          <projectDescription>
+            <p></p>
+          </projectDescription>
+          <projectLinks>
+            
+          </projectLinks>
+        </projectContent>
+      </projectsContainer>
     </MainDiv>
   )
 }
