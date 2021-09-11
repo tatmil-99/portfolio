07605e670b77dd3ff0b49e7bf8a2adc2520d30e6
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-09-11 18:59:22 -0500

    adds more styling to project card

--- src/components/ProjectDescription.js
+++ src/components/ProjectDescription.js
@@ -9,6 +9,7 @@ const FlexExtended = styled.div`
 `;
 
 const ProjectDescriptionDiv = styled(FlexExtended)`
+
   p {
     color: rgb(107, 122, 144);
     padding: 20px 0 20px;
@@ -17,10 +18,21 @@ const ProjectDescriptionDiv = styled(FlexExtended)`
     line-height: 1.5;
     font-size: 1em;
   }
-  
+
   ul {
-    ${props => props.FlexExtended}
+    /* display: flex;
     flex-direction: row;
+    flex-wrap: wrap;
+    list-style:none;
+    justify-content: center;
+    padding: 0; */
+    float: left;
+    list-style: none;
+    padding: 0;
+    word-spacing: 5px;
+    line-height: 1.3;
+    margin: 5px 0 5px;
+    color: rgb(252,15,192);
   }
 `;
 
