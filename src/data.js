925cbeff42ef248d23d20a2897d2f0108690fe9c
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-09-13 22:36:39 -0500

    removes dummy data

--- src/data.js
+++ src/data.js
@@ -6,11 +6,5 @@ export const projects = [
     technologies: ['React', 'Redux', 'StyledComponents', 'JavaScript'],
     link: 'https://github.com/tatmil-99/The-Bean',
   },
-  {
-    title: 'The Bean',
-    description: 
-      'A e-commerce web app that sells coffee beans. Made to mimic realworld apps and practice data management.',
-    technologies: ['React', 'Redux', 'StyledComponents', 'JavaScript'],
-    link: 'https://github.com/tatmil-99/The-Bean',
-  },
+  
 ]
