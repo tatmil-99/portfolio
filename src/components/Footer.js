b5d5ca05565c5773e916d085f1a47321998c0e48
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-09-24 00:09:48 -0500

    maps link data and displays social media icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import {socialLinks} from '../data';
+import { ExternalLink } from 'react-external-link';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
-const FooterDiv = styled.div`
-  background-color: rgb(47, 48, 59);
+const FlexExtended = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const FooterDiv = styled(FlexExtended)`
+  background-color: rgb(47, 48, 59);
   width: 100%;
   margin: 0px 0px 0px 0px;
   color: rgb(107, 122, 144);
@@ -15,15 +23,70 @@ const FooterDiv = styled.div`
     margin-bottom: 15px;
   } */
   
-  p {
+  /* p {
     margin: 50px 50px;
-  }
+  } */
 `;
 
+const LinksContainer = styled(FlexExtended)`
+  flex-direction: row;
+`;
+
+const CreditsContainer = styled(FlexExtended)``;
+
+const MyGitHubIcon = styled(GitHubIcon)`
+  color: rgb(107, 122, 144);
+`;
+
+const MyTwitterIcon = styled(TwitterIcon)`
+  color: rgb(107, 122, 144);
+`;
+
+const MyLinkedInIcon = styled(LinkedInIcon)`
+  color: rgb(107, 122, 144);
+`;
+
+
 const Footer = () => {
+
   return (
     <FooterDiv>
-      <p>Created by Tatien Miller</p>
+      <LinksContainer>
+        {
+          socialLinks.map((link) => {
+            switch (link.account) {
+              case 'GitHub':
+
+                return (
+                  <ExternalLink href={link.link}>
+                    <MyGitHubIcon style={{fontSize: 30}} />
+                  </ExternalLink>
+                );
+              case 'Twitter':
+
+                return (
+                  <ExternalLink href={link.link}>
+                    <MyTwitterIcon style={{fontSize: 30}} />
+                  </ExternalLink>
+                );
+              case 'LinkedIn':
+
+                return (
+                  <ExternalLink href={link.link}>
+                    <MyLinkedInIcon style={{fontSize: 30}} />
+                  </ExternalLink>
+                );
+              default:
+                return (
+                  null
+                );
+            }
+          })
+        }
+      </LinksContainer>
+      <CreditsContainer>
+        <p>Designed & Built by Tatien Miller</p>
+      </CreditsContainer>
     </FooterDiv>
   );
 }
