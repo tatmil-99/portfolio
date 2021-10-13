b1fb86a4187df08da0a29d9d029324676c2e33f7
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-10-12 21:50:00 -0500

    passes test for finding name and email input in dom

--- src/ContactForm.test.js
+++ src/ContactForm.test.js
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import ContactForm from './components/ContactForm';
 
 describe('ContactForm', () => {
   test('renders Form component', () => {
     render(<ContactForm />);
 
-    screen.getByRole('textbox');
+    const nameInput = screen.getByPlaceholderText('Name');
+    const emailInput = screen.getByPlaceholderText('Email address');
+
+    expect(nameInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
   });
 });
