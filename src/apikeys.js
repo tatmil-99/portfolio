65615458b6337edaf1c0c1b8e173139843ed1c7c
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-11-10 01:29:23 -0600

    adds apikeys

---
+++ src/apikeys.js
@@ -0,0 +1,5 @@
+export default {
+  USER_ID : 'user_oKL1r6zuxCWVGqRNxjrNv',
+  TEMPLATE_ID : 'contact_form',
+  SERVICE_ID : 'contact_service'
+}
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,6 +5,7 @@ import emailjs from 'emailjs-com';
 import { ToastContainer, toast } from 'react-toastify';
 import{ init } from 'emailjs-com';
 import 'react-toastify/dist/ReactToastify.min.css';
+import apikeys from '../apikeys';
 init("REACT_APP_USER_ID");
 
 const FlexExtended = styled.div`
@@ -151,10 +152,13 @@ const ContactForm = () => {
       }
 
       await emailjs.send(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_TEMPLATE_ID,
+        // process.env.REACT_APP_SERVICE_ID,
+        // process.env.REACT_APP_TEMPLATE_ID,
+        apikeys.SERVICE_ID,
+        apikeys.TEMPLATE_ID,
         templateParams,
-        process.env.REACT_APP_USER_ID
+        apikeys.USER_ID
+        // process.env.REACT_APP_USER_ID
       );
       reset();
       toastifySuccess();
