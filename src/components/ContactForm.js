5b297c8fcdbd0de6047a47396baa6a05d0aff951
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-11-09 22:11:06 -0600

    changes emailjs.send to sendForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -150,7 +150,7 @@ const ContactForm = () => {
         message
       }
 
-      await emailjs.send(
+      await emailjs.sendForm(
         process.env.REACT_APP_SERVICE_ID,
         process.env.REACT_APP_TEMPLATE_ID,
         templateParams,
